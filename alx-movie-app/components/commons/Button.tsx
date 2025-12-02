62f5a0de364ef25d95397df2494f58caa597aa02
Otega Otite <otiteotega@gmail.com>
2025-12-02 18:41:51 +0100

    Bootstrap MovieApp

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,5 +1,5 @@
 
-const Buttons: React.FC =() => {
+const Button: React.FC =() => {
 
     return (
         <div>
@@ -7,4 +7,4 @@ const Buttons: React.FC =() => {
         </div>
     )
 }
-export default Buttons;
+export default Button;
